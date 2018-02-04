9be01e600c3578af5e0d7bd19786644334c65ff1
M-Yankov <cska31ultras@gmail.com>
2018-02-04 18:41:47 +0200

    Added more files to the repository

---
+++ Sitefinity.LibraryItemsDownloader/Custom/Services/ILibraryItemsDownloadService.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Sitefinity.LibraryItemsDownloader.Custom.Services
+{
+    [ServiceContract]
+    public interface ILibraryItemsDownloadService
+    {
+        [WebInvoke(Method = "POST", UriTemplate = "/DownloadImages", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        string DownloadImages(string[] imageIds);
+    }
+}
---
+++ Sitefinity.LibraryItemsDownloader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitefinity.LibraryItemsDownloader
+{
+    public class Program
+    {
+        public void Main()
+        {
+            Console.WriteLine(typeof(Program).Assembly.FullName);
+        }
+    }
+}
