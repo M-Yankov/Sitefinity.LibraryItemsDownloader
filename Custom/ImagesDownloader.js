e86c6426bc016b76460c1846bf1a84bf8a0ae422
M-Yankov <cska31ultras@gmail.com>
2018-02-04 18:13:27 +0200

    Add project files.

---
+++ Custom/ImagesDownloader.js
@@ -0,0 +1,84 @@
+﻿var my_binder;
+
+// called by the MasterGridView when it is loaded
+// the sender here is MasterGridView
+function OnMasterViewLoadedCustom(sender, args) {
+
+    // sender.add_itemCommand(handleMasterGridViewItemCommand);
+
+    
+    // .get_selectedItems()
+
+    my_binder = sender.get_binder();
+
+     var itemsGrid = sender.get_currentItemsList();
+     itemsGrid.add_command(onMyCommand);
+    // itemsGrid.add_beforeCommand(handleItemsGridBeforeCommand);
+     // itemsGrid.add_itemCommand(onMyItemCommand);
+    // itemsGrid.add_dialogClosed(handleItemsGridDialogClosed);
+}
+
+function onMyCommand(sender, args) {
+
+    var commandName = args.get_commandName();
+    if (commandName !== 'DownloadSelectedImages') {
+        return;
+    }
+
+    var items = my_binder.get_selectedItems();
+    if (!items || items.length < 1) {
+        alert('Please select items!');
+    }
+
+
+    var dataItem = sender.get_dataItem();
+    var zipFileName = 'Images';
+    if (dataItem !== null) {
+        zipFileName = dataItem.LibraryTitle;
+    }
+    var imageIds = items.map(function (item) {
+        return item.Id;
+    });
+
+    var data = JSON.stringify(imageIds);
+    $.ajax({
+        method: 'POST',
+        url: '/LibrariesService/DownloadImages',
+        data: data,
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data) {
+            suc(data, zipFileName);
+        },
+        error: function (e) {
+            console.error(e);
+        }
+    });
+    
+}
+
+function base64ToArrayBuffer(base64) {
+    var binaryString = window.atob(base64);
+    var binaryLen = binaryString.length;
+    var bytes = new Uint8Array(binaryLen);
+    for (var i = 0; i < binaryLen; i++) {
+        var ascii = binaryString.charCodeAt(i);
+        bytes[i] = ascii;
+    }
+    return bytes;
+}
+
+function suc(byteArray, zipFileName) {
+    var data = base64ToArrayBuffer(byteArray);
+    var blob = new Blob([ data ], { type: 'application/zip' });
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    var fileName = zipFileName + '.zip';
+    link.download = fileName;
+    link.click();
+}
+
+function onMyItemCommand(sender, args) {
+    console.log('OnItemCommand');
+    return true;
+}
---
+++ Custom/Services/LibraryItemsDownloadService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Web;
+using Telerik.Sitefinity.Modules.Libraries;
+using Telerik.Sitefinity.Utilities.Zip;
+using Telerik.Sitefinity.Libraries.Model;
+using Telerik.Sitefinity.Configuration;
+
+namespace SitefinityWebApp.Custom.Services
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true, InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Single)]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class LibraryItemsDownloadService : ILibraryItemsDownloadService
+    {
+        public string DownloadImages(string[] imageIds)
+        {
+            string result = string.Empty;
+            var libraryManager = LibrariesManager.GetManager();
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (ZipFile zipFiles = new ZipFile())
+            {
+                foreach (string id in imageIds ?? Enumerable.Empty<string>())
+                {
+                    Guid imageId;
+                    if (Guid.TryParse(id, out imageId))
+                    {
+                        Image image = libraryManager.GetImage(imageId);
+                        if (image != null)
+                        {
+                            Image liveImage = libraryManager.Provider.GetLiveBase<Image>(image);
+                            Stream stream = libraryManager.Download(liveImage);
+                            string imageName = Path.GetFileName(image.FilePath);
+                            zipFiles.AddFileStream(imageName, string.Empty, stream);
+                        }
+                    };
+                }
+
+                zipFiles.Save(memoryStream);
+
+                byte[] byteResult = memoryStream.ToArray();
+                result = Convert.ToBase64String(byteResult);
+            }
+
+            return result;
+        }
+    }
+}
