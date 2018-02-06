e6df465fdd8053f8ecdefa71028dfa8af38f1014
M-Yankov <cska31ultras@gmail.com>
2018-02-07 00:15:30 +0200

    Started working on adding DownloadSelecteImages command in Toolbar

--- Sitefinity.LibraryItemsDownloader/Helpers/InstallationsHelper.cs
+++ Sitefinity.LibraryItemsDownloader/Helpers/InstallationsHelper.cs
@@ -8,7 +8,9 @@ using Telerik.Sitefinity.Configuration;
 using Telerik.Sitefinity.Data;
 using Telerik.Sitefinity.Modules.Libraries.Configuration;
 using Telerik.Sitefinity.Services;
+using Telerik.Sitefinity.Web.UI.Backend.Elements.Config;
 using Telerik.Sitefinity.Web.UI.ContentUI.Config;
+using Telerik.Sitefinity.Web.UI.ContentUI.Views.Backend.Master.Config;
 
 namespace Sitefinity.LibraryItemsDownloader.Helpers
 {
@@ -41,6 +43,9 @@ namespace Sitefinity.LibraryItemsDownloader.Helpers
                     return; // Error; 
                 }
                 string key = Assembly.CreateQualifiedName(ass.FullName, fullNamespaceJavaScriptFile);
+                MasterGridViewElement masterV = view as MasterGridViewElement;
+                WidgetBarElement toolbar = (masterV.Toolbar as WidgetBarElement);
+                //TODO: var moreActionWidget = toolbar.Sections.Elements.FirstOrDefault(w => w.Name == "MoreActionsWidget");
 
                 if (!view.Scripts.ContainsKey(key))
                 {
@@ -52,6 +57,11 @@ namespace Sitefinity.LibraryItemsDownloader.Helpers
                     // In initialize the context is not authenticated to save.
                     manager.SaveSection(a.Section, true);
                 }
+
+                #region Toolbar
+                ConfigProperty dd = view.Properties.FirstOrDefault(p => p.Name == "toolbar");
+                // WidgetBarConfigElement wb = (WidgetBarElement)dd;
+                #endregion
             }
         }
     }
