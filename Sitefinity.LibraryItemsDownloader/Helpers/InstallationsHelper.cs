30bdb25f5cd7b20e5e93ac59b2fe1a9b9c172b1f
M-Yankov <cska31ultras@gmail.com>
2018-02-10 19:05:31 +0200

    Modified InstallationsHelper to reuse functionality for Videos and Documents. Added extended configs for Videos and Documents. #4

--- Sitefinity.LibraryItemsDownloader/Helpers/InstallationsHelper.cs
+++ Sitefinity.LibraryItemsDownloader/Helpers/InstallationsHelper.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.UI;
 using Telerik.Sitefinity.Configuration;
 using Telerik.Sitefinity.Data;
 using Telerik.Sitefinity.Modules.Libraries.Configuration;
-using Telerik.Sitefinity.Services;
+using Telerik.Sitefinity.Modules.Libraries.Documents;
+using Telerik.Sitefinity.Modules.Libraries.Images;
+using Telerik.Sitefinity.Modules.Libraries.Videos;
 using Telerik.Sitefinity.Web.UI.Backend.Elements.Config;
+using Telerik.Sitefinity.Web.UI.Backend.Elements.Enums;
 using Telerik.Sitefinity.Web.UI.Backend.Elements.Widgets;
 using Telerik.Sitefinity.Web.UI.ContentUI.Config;
 using Telerik.Sitefinity.Web.UI.ContentUI.Views.Backend.Master.Config;
@@ -18,123 +18,95 @@ namespace Sitefinity.LibraryItemsDownloader.Helpers
 {
     public class InstallationsHelper
     {
-        public static void Initialize()
+        private const string WidgetBarSectionName = "toolbar";
+        private const string MoreActionsWidgetName = "MoreActionsWidget";
+        private const string DownloadSelectedImagesCommandName = "DownloadSelectedImages";
+        private const string DownloadSelectedVideosCommandName = "DownloadSelectedVideos";
+        private const string DownloadSelectedDocumentsCommandName = "DownloadSelectedVideos";
+
+        public void Initialize()
         {
-            var manager = ConfigManager.GetManager();
+            ConfigManager manager = ConfigManager.GetManager();
 
             // In initialize the context is not authenticated to save.
             using (ElevatedModeRegion elevatedMode = new ElevatedModeRegion(manager))
             {
-                var libsConfig = manager.GetSection<LibrariesConfig>();
-                if (!libsConfig.ContentViewControls.ContainsKey("ImagesBackend"))
-                {
-                    return;// Error!;
-                }
-
-                var a = libsConfig.ContentViewControls["ImagesBackend"];
-                if (!a.ContainsView("ImagesBackendList"))
-                {
-                    return; // Error; 
-                }
-
-                var view = a.ViewsConfig["ImagesBackendList"];
-
-                var ass = typeof(Installer).Assembly;
-                var fullNamespaceJavaScriptFile = ass.GetManifestResourceNames().FirstOrDefault(fileName => fileName.EndsWith("LibraryItemsDownloadService.js"));
-                if (fullNamespaceJavaScriptFile == null)
-                {
-                    return; // Error; 
-                }
-
-                string key = Assembly.CreateQualifiedName(ass.FullName, fullNamespaceJavaScriptFile);
-
-                #region Scripts
-                // var moreActionWidget = toolbar.Sections.Elements.FirstOrDefault(w => w.Name == "MoreActionsWidget");
-                // ActionMenuWidgetElement
-                if (!view.Scripts.ContainsKey(key))
-                {
-                    var kc = new ClientScriptElement(view.Scripts);
-                    kc.ScriptLocation = key;
-                    kc.LoadMethodName = "OnMasterViewLoadedCustom";
-                    view.Scripts.Add(kc);
-
-
-                    manager.SaveSection(a.Section, true);
-                }
-                #endregion
-
-                #region Toolbar
-                MasterGridViewElement masterV = view as MasterGridViewElement;
-                // masterV.ToolbarConfig.Sections.Add(new WidgetBarSectionElement(null));
-
-                WidgetBarElement toolbarConfig = (masterV.ToolbarConfig as WidgetBarElement);
-
-                WidgetBarSectionElement widgetBarSectionElement = toolbarConfig.Sections.OfType<WidgetBarSectionElement>().FirstOrDefault(s => s.Name == "toolbar");
-                ActionMenuWidgetElement moreActionsWidgetElement = widgetBarSectionElement.Items.OfType<ActionMenuWidgetElement>().FirstOrDefault(w => w.Name == "MoreActionsWidget");
-                CommandWidgetElement downloadSelectedImagesCommand = moreActionsWidgetElement.MenuItems.OfType<CommandWidgetElement>().FirstOrDefault(c => c.Name == "DownloadSelectedImages");
-
-                if (downloadSelectedImagesCommand == null)
-                {
-                    downloadSelectedImagesCommand = new CommandWidgetElement(moreActionsWidgetElement.MenuItems);
-                    downloadSelectedImagesCommand.Name = "DownloadSelectedImages";
-                    downloadSelectedImagesCommand.CssClass = "sfDownloadSelectedImages sfDownloadItm";
-                    downloadSelectedImagesCommand.ButtonType = Telerik.Sitefinity.Web.UI.Backend.Elements.Enums.CommandButtonType.Standard;
-                    downloadSelectedImagesCommand.WrapperTagId = System.Web.UI.HtmlTextWriterTag.Li.ToString();
-                    downloadSelectedImagesCommand.WrapperTagKey = System.Web.UI.HtmlTextWriterTag.Li;
-                    downloadSelectedImagesCommand.WidgetType = typeof(CommandWidget);
-                    downloadSelectedImagesCommand.CommandName = "DownloadSelectedImages";
-                    downloadSelectedImagesCommand.Text = "Download Selected Images";
-
-                    moreActionsWidgetElement.MenuItems.Add(downloadSelectedImagesCommand);
-                    manager.SaveSection(a.Section, true);
-                }
-
-                #endregion
+                LibrariesConfig libsConfig = manager.GetSection<LibrariesConfig>();
+                this.ConfigureSection(manager, libsConfig, ImagesDefinitions.BackendImagesDefinitionName, ImagesDefinitions.BackendListViewName, DownloadSelectedImagesCommandName, "Download selected images");
+                this.ConfigureSection(manager, libsConfig, VideosDefinitions.BackendVideosDefinitionName, VideosDefinitions.BackendListViewName, DownloadSelectedVideosCommandName, "Download selected videos");
+                this.ConfigureSection(manager, libsConfig, DocumentsDefinitions.BackendDefinitionName, DocumentsDefinitions.BackendListViewName, DownloadSelectedDocumentsCommandName, "Download selected documents");
             }
         }
 
-        private static string GetConfigAsString(CommandWidgetElement widget)
+        public void ConfigureSection(ConfigManager manager, LibrariesConfig libsConfig, string definitionName, string backendListViewName, string commandName, string commandText)
         {
-            var sb = new StringBuilder();
-            sb.AppendFormat("\nActionName: {0}", widget.ActionName);
-            sb.AppendFormat("\nButtonCssClass: {0}", widget.ButtonCssClass);
-            sb.AppendFormat("\nButtonType: {0}", widget.ButtonType);
-            sb.AppendFormat("\nCollectionItemName: {0}", widget.CollectionItemName);
-            sb.AppendFormat("\nCommandArgument: {0}", widget.CommandArgument);
-            sb.AppendFormat("\nCommandName: {0}", widget.CommandName);
-            sb.AppendFormat("\nCondition: {0}", widget.Condition);
-            sb.AppendFormat("\nContainerId: {0}", widget.ContainerId);
-            sb.AppendFormat("\nCssClass: {0}", widget.CssClass);
-            sb.AppendFormat("\nGetKey(): {0}", widget.GetKey());
-            sb.AppendFormat("\nGetPath(): {0}", widget.GetPath());
-            sb.AppendFormat("\nIsFilterCommand:{0}", widget.IsFilterCommand);
-            sb.AppendFormat("\nIsSeparator: {0}", widget.IsSeparator);
-            sb.AppendFormat("\nModuleName: {0}", widget.ModuleName);
-            sb.AppendFormat("\nName: {0}", widget.Name);
-            sb.AppendFormat("\nNavigateUrl: {0}", widget.NavigateUrl);
-            sb.AppendFormat("\nObjectProviderName: {0}", widget.ObjectProviderName);
-            sb.AppendFormat("\nOpenInSameWindow: {0}", widget.OpenInSameWindow);
-            sb.AppendFormat("\nParent: {0}", widget.Parent != null ? widget.Parent.TagName : "Null");
-            sb.AppendFormat("\nPermissionSet: {0}", widget.PermissionSet);
-            sb.AppendFormat("\nProperties: [\"{0}\"],", string.Join("\",\"", (widget.Properties ?? Enumerable.Empty<ConfigProperty>()).Select(p => p.Name)));
-            sb.AppendFormat("\nRelatedSecuredObjectId: {0}", widget.RelatedSecuredObjectId);
-            sb.AppendFormat("\nRelatedSecuredObjectManagerTypeName: {0}", widget.RelatedSecuredObjectManagerTypeName);
-            sb.AppendFormat("\nRelatedSecuredObjectProviderName: {0}", widget.RelatedSecuredObjectProviderName);
-            sb.AppendFormat("\nRelatedSecuredObjectTypeName: {0}", widget.RelatedSecuredObjectTypeName);
-            sb.AppendFormat("\nRequiredActions: {0}", widget.RequiredActions);
-            sb.AppendFormat("\nRequiredPermissionSet: {0}", widget.RequiredPermissionSet);
-            sb.AppendFormat("\nResourceClassId: {0}", widget.ResourceClassId);
-            sb.AppendFormat("\nSection: {0}", widget.Section != null ? widget.Section.TagName : "NUll");
-            sb.AppendFormat("\nTagName: {0}", widget.TagName);
-            sb.AppendFormat("\nText: {0}", widget.Text);
-            sb.AppendFormat("\nToolTip: {0}", widget.ToolTip);
-            sb.AppendFormat("\nVisible: {0}", widget.Visible);
-            sb.AppendFormat("\nWidgetType: {0}", widget.WidgetType != null ? widget.WidgetType.FullName : "NULL");
-            sb.AppendFormat("\nWidgetVirtualPath: {0}", widget.WidgetVirtualPath);
-            sb.AppendFormat("\nWrapperTagId: {0}", widget.WrapperTagId);
-            sb.AppendFormat("\nWrapperTagKey: {0}", widget.WrapperTagKey);
+            if (!libsConfig.ContentViewControls.ContainsKey(definitionName))
+            {
+                string exceptionMessage = string.Format("Cannot find content view control: {0} related with libraries configuration.", definitionName);
+                throw new NullReferenceException(exceptionMessage);
+            }
 
-            return sb.ToString();
+            ContentViewControlElement imagesBackend = libsConfig.ContentViewControls[definitionName];
+            if (!imagesBackend.ContainsView(backendListViewName))
+            {
+                string exceptionMessage = string.Format("Cannot find back-end view: {0}.", backendListViewName);
+                throw new NullReferenceException(exceptionMessage);
+            }
+
+            ContentViewDefinitionElement view = imagesBackend.ViewsConfig[backendListViewName];
+
+            Assembly itemsDownloaderAssembly = typeof(Installer).Assembly;
+            string javascriptKey = this.GetJavaScriptQualifiedNameKey(itemsDownloaderAssembly);
+
+            #region Scripts
+            if (!view.Scripts.ContainsKey(javascriptKey))
+            {
+                var scriptElement = new ClientScriptElement(view.Scripts);
+                scriptElement.ScriptLocation = javascriptKey;
+                scriptElement.LoadMethodName = "OnMasterViewLoadedCustom";
+                view.Scripts.Add(scriptElement);
+
+                manager.SaveSection(imagesBackend.Section, true);
+            }
+            #endregion
+
+            #region Toolbar
+            MasterGridViewElement masterV = view as MasterGridViewElement;
+
+            WidgetBarElement toolbarConfig = (masterV.ToolbarConfig as WidgetBarElement);
+            WidgetBarSectionElement widgetBarSectionElement = toolbarConfig.Sections.OfType<WidgetBarSectionElement>().FirstOrDefault(s => s.Name == WidgetBarSectionName);
+            ActionMenuWidgetElement moreActionsWidgetElement = widgetBarSectionElement.Items.OfType<ActionMenuWidgetElement>().FirstOrDefault(w => w.Name == MoreActionsWidgetName);
+            CommandWidgetElement downloadSelectedImagesCommand = moreActionsWidgetElement.MenuItems.OfType<CommandWidgetElement>().FirstOrDefault(c => c.Name == commandName);
+
+            if (downloadSelectedImagesCommand == null)
+            {
+                downloadSelectedImagesCommand = new CommandWidgetElement(moreActionsWidgetElement.MenuItems);
+                downloadSelectedImagesCommand.Name = commandName;
+                downloadSelectedImagesCommand.CssClass = string.Format("sf{0} sfDownloadItm", commandName);
+                downloadSelectedImagesCommand.ButtonType = CommandButtonType.Standard;
+                downloadSelectedImagesCommand.WrapperTagId = HtmlTextWriterTag.Li.ToString();
+                downloadSelectedImagesCommand.WrapperTagKey = HtmlTextWriterTag.Li;
+                downloadSelectedImagesCommand.WidgetType = typeof(CommandWidget);
+                downloadSelectedImagesCommand.CommandName = commandName;
+                downloadSelectedImagesCommand.Text = commandText;
+
+                moreActionsWidgetElement.MenuItems.Add(downloadSelectedImagesCommand);
+                manager.SaveSection(imagesBackend.Section, true);
+            }
+
+            #endregion
+        }
+
+        public string GetJavaScriptQualifiedNameKey(Assembly libraryItemsDownloaderAssembly)
+        {
+            var fullNamespaceJavaScriptFile = libraryItemsDownloaderAssembly.GetManifestResourceNames().FirstOrDefault(fileName => fileName.EndsWith("LibraryItemsDownloadService.js"));
+            if (fullNamespaceJavaScriptFile == null)
+            {
+                throw new NullReferenceException("Cannot find embedded file LibraryItemsDownloadService.js");
+            }
+
+            string javaScriptQualifiedNameKey = Assembly.CreateQualifiedName(libraryItemsDownloaderAssembly.FullName, fullNamespaceJavaScriptFile);
+            return javaScriptQualifiedNameKey;
         }
     }
 }
--- Sitefinity.LibraryItemsDownloader/Installer.cs
+++ Sitefinity.LibraryItemsDownloader/Installer.cs
@@ -1,14 +1,6 @@
 ﻿using Sitefinity.LibraryItemsDownloader.Custom.Services;
 using Sitefinity.LibraryItemsDownloader.Helpers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Telerik.Sitefinity.Configuration;
-using Telerik.Sitefinity.Data;
-using Telerik.Sitefinity.Modules.Libraries.Configuration;
 using Telerik.Sitefinity.Services;
 
 namespace Sitefinity.LibraryItemsDownloader
@@ -23,7 +15,7 @@ namespace Sitefinity.LibraryItemsDownloader
         private static void ApplicationStartHandler(object sender, EventArgs e)
         {
             SystemManager.RegisterWebService(typeof(LibraryItemsDownloadService), LibraryItemsDownloadService.WebServicePath);
-            InstallationsHelper.Initialize();
+            new InstallationsHelper().Initialize();
         }
     }
 }
