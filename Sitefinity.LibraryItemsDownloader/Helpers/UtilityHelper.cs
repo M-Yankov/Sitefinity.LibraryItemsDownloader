a834f85e38a544ffbabaf51398f621d59e6c22f9
M-Yankov <cska31ultras@gmail.com>
2018-02-22 22:44:06 +0200

    Closes #12 Added logic to handle special symbols when downloading a library

---
+++ Sitefinity.LibraryItemsDownloader/Helpers/UtilityHelper.cs
@@ -0,0 +1,37 @@
+﻿namespace Sitefinity.LibraryItemsDownloader.Helpers
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class UtilityHelper : IUtilityHelper
+    {
+        private const string DefaultReplaceCharacter = "_";
+        HashSet<char> reservedSymbols = null;
+
+        public UtilityHelper()
+        {
+            IEnumerable<char> specialSymbols = Path.GetInvalidFileNameChars().Union(Path.GetInvalidPathChars()).ToList();
+            this.reservedSymbols = new HashSet<char>(specialSymbols);
+        }
+
+        public string ReplaceInvlaidCharacters(string text, string replaceCharacter = DefaultReplaceCharacter)
+        {
+            StringBuilder textResult = new StringBuilder();
+            foreach (char currentSymbol in text)
+            {
+                if (this.reservedSymbols.Contains(currentSymbol))
+                {
+                    textResult.Append(replaceCharacter);
+                }
+                else
+                {
+                    textResult.Append(currentSymbol);
+                }
+            }
+
+            return textResult.ToString();
+        }
+    }
+}
