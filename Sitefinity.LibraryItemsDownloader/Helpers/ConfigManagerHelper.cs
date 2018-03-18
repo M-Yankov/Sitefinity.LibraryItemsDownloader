61ca74e087621a40c9f406bde52b1a675aa9dadd
M-Yankov <cska31ultras@gmail.com>
2018-03-18 18:17:42 +0200

    Added config manager helper to make testing easier. Added More validations

---
+++ Sitefinity.LibraryItemsDownloader/Helpers/ConfigManagerHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace Sitefinity.LibraryItemsDownloader.Helpers
+{
+    using Telerik.Sitefinity.Configuration;
+    using Telerik.Sitefinity.Data;
+
+    public class ConfigManagerHelper : IConfigManagerHelper
+    {
+        private readonly ConfigManager configManager;
+
+        public ConfigManagerHelper(ConfigManager configManager)
+        {
+            this.configManager = configManager;
+        }
+
+        public IManager Manager
+        {
+            get
+            {
+                return this.configManager;
+            }
+        }
+
+        public TSection GetSection<TSection>() where TSection : ConfigSection, new()
+        {
+            return this.configManager.GetSection<TSection>();
+        }
+
+        public void SaveSection(ConfigSection section, bool useFileSystemMode)
+        {
+            this.configManager.SaveSection(section, useFileSystemMode);
+        }
+    }
+}
