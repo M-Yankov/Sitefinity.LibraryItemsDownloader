94bb2ac068b8cb0f632060d9afa6aa4d40023e41
M-Yankov <cska31ultras@gmail.com>
2018-03-22 22:55:28 +0200

    #17 Added more tests. Not ready. Needs refactoring.

--- Sitefinity.LibraryItemsDownloader.Tests/Services/LibraryItemsDownloadServiceTests.cs
+++ Sitefinity.LibraryItemsDownloader.Tests/Services/LibraryItemsDownloadServiceTests.cs
@@ -19,6 +19,7 @@ using Telerik.Sitefinity.Security.Model;
 using Telerik.Sitefinity.Utilities.Zip;
 using Sitefinity.LibraryItemsDownloader.Helpers;
 using System.IO;
+using Telerik.Sitefinity;
 
 namespace Sitefinity.LibraryItemsDownloader.Tests.Services
 {
@@ -26,16 +27,17 @@ namespace Sitefinity.LibraryItemsDownloader.Tests.Services
     {
         private const string ReturnTestResult = "TestingContent";
         private Mock<LibraryItemsDownloadService> libraryItemsDownloadService;
-        
+        private Mock<ILibraryManagerHelper> managerHelper;
+
         public readonly Action DoNothing = () => { };
 
         [SetUp]
         public void Initialize()
         {
             Mock<IUtilityHelper> utilityHelperMock = new Mock<IUtilityHelper>();
-            Mock<ILibraryManagerHelper> managerHelper = new Mock<ILibraryManagerHelper>();
+            this.managerHelper = new Mock<ILibraryManagerHelper>();
 
-            this.libraryItemsDownloadService = new Mock<LibraryItemsDownloadService>(utilityHelperMock.Object, managerHelper.Object);
+            this.libraryItemsDownloadService = new Mock<LibraryItemsDownloadService>(utilityHelperMock.Object, this.managerHelper.Object);
             this.libraryItemsDownloadService.CallBase = true;
 
             this.libraryItemsDownloadService
@@ -50,7 +52,7 @@ namespace Sitefinity.LibraryItemsDownloader.Tests.Services
             this.libraryItemsDownloadService
                 .Setup(this.GetDownloadableContentMethodExpession<Image>())
                 .Returns(ReturnTestResult);
-            
+
             // Act 
             string actualResult = this.libraryItemsDownloadService.Object.DownloadImages(Enumerable.Empty<DownloadLibaryItemRequestModel>());
 
@@ -59,7 +61,6 @@ namespace Sitefinity.LibraryItemsDownloader.Tests.Services
             this.libraryItemsDownloadService
                 .Verify(this.GetDownloadableContentMethodExpession<Image>(), Times.Once());
         }
-
 
         [Test]
         public void ExpectDownloadVideosToReturnCorrectResults()
@@ -123,14 +124,135 @@ namespace Sitefinity.LibraryItemsDownloader.Tests.Services
             Assert.AreEqual(expectedData, zipFileBinaryData);
         }
 
-        private string GetTestZipStreamBytesAsText(string testData = null)
+        [Test]
+        public void ExpectGetDownloadableContentToSaveOneSelectedFile()
+        {
+            // Arrange
+            List<DownloadLibaryItemRequestModel> guids = new List<DownloadLibaryItemRequestModel>();
+            guids.Add(new DownloadLibaryItemRequestModel() { Id = "55DDA8C6-4270-4092-8196-BF62631FB97A", IsFolder = false });
+
+            FileZipModel testFileZip = new FileZipModel("TestDataFile", "FileName.txt", string.Empty);
+
+            this.libraryItemsDownloadService
+                .Setup(this.GetSaveLibraryItemsToStreamMethodExpressionWithListFilter<Video>(guids.Count))
+                .Callback<IEnumerable<Guid>, ZipFile, string>((guidIds, zipStream, directory) =>
+               {
+                   ZipEntry zipEntry = zipStream.AddFileFromString(testFileZip.FileNameWithExtension, testFileZip.DirectoryInZip, testFileZip.Content);
+                   testFileZip.LastModified = zipEntry.LastModified;
+               });
+
+            // Act
+            string result = this.libraryItemsDownloadService.Object.GetDownloadableContent<Video>(guids);
+
+            // Assert
+            this.libraryItemsDownloadService
+              .Verify(this.GetSaveLibraryItemsToStreamMethodExpressionWithListFilter<Video>(guids.Count), Times.Once());
+
+            string expectedData = this.GetTestZipStreamBytesAsText(testFileZip);
+
+            Assert.AreEqual(expectedData, result);
+        }
+
+        [Test]
+        public void ExpectGetDownloadableContentToSaveOneSelectedFolderAndOneSelectedFile()
+        {
+            // Arrange
+            List<DownloadLibaryItemRequestModel> guids = new List<DownloadLibaryItemRequestModel>();
+            guids.Add(new DownloadLibaryItemRequestModel() { Id = "55DDA8C6-4270-4092-8196-BF62631FB97A", IsFolder = false });
+            guids.Add(new DownloadLibaryItemRequestModel() { Id = "55DDA8C6-4270-4092-8196-BF62631FB97B", IsFolder = true });
+
+            FileZipModel testFileZip = new FileZipModel("TestDataFile", "FileName.txt", string.Empty);
+            FileZipModel testFileInFolderZip = new FileZipModel("var a = 5;", "Documentation.pdf", "TestFolder");
+
+            Mock<IFolder> folderMock = new Mock<IFolder>();
+            this.managerHelper
+                .Setup(helper => helper.GetFolder(It.IsAny<Guid>()))
+                .Returns(folderMock.Object);
+
+            int notFolderItemsCount = guids.Count(model => !model.IsFolder);
+
+            this.libraryItemsDownloadService
+                .Setup(this.GetSaveLibraryItemsToStreamMethodExpressionWithListFilter<Image>(notFolderItemsCount))
+                .Callback<IEnumerable<Guid>, ZipFile, string>((guidIds, zipStream, directory) =>
+                 {
+                     ZipEntry zipEntry = zipStream.AddFileFromString(testFileZip.FileNameWithExtension, testFileZip.DirectoryInZip, testFileZip.Content);
+                     testFileZip.LastModified = zipEntry.LastModified;
+                 });
+
+            this.libraryItemsDownloadService
+                .Setup(this.GetSaveLibraryItemsToStreamRecursively<Image>())
+                .Callback<IFolder, ZipFile, string>((folder, zipStream, directory) =>
+                {
+                    ZipEntry zipEntryFolder = zipStream.AddFileFromString(testFileInFolderZip.FileNameWithExtension, testFileInFolderZip.DirectoryInZip, testFileInFolderZip.Content);
+                    testFileInFolderZip.LastModified = zipEntryFolder.LastModified;
+                });
+
+            // Act
+            string result = this.libraryItemsDownloadService.Object.GetDownloadableContent<Image>(guids);
+
+            // Assert
+            this.libraryItemsDownloadService
+                .Verify(this.GetSaveLibraryItemsToStreamMethodExpressionWithListFilter<Image>(notFolderItemsCount), Times.Once());
+
+            this.libraryItemsDownloadService
+                .Verify(this.GetSaveLibraryItemsToStreamRecursively<Image>(), Times.Once());
+
+            string expectedData = this.GetTestZipStreamBytesAsText(testFileInFolderZip, testFileZip);
+
+            Assert.AreEqual(expectedData, result);
+        }
+
+        [Test]
+        public void ExpectGetDownloadableContentToSaveSelectedFoldersOnly()
+        {
+
+        }
+
+        [Test]
+        public void ExpectGetDownloadableContentTo_WellIDoNotKnowWhatToExpect_WhenOnlyFoldersAreSelectedButTheyDoNoHaveAnything()
+        {
+
+        }
+
+        [Ignore("For Later")]
+        public void WhenHelperCannotFindTheFolder()
+        {
+
+        }
+
+        public class FileZipModel
+        {
+            public FileZipModel(string content, string filename, string directory)
+            {
+                this.Content = content;
+                this.FileNameWithExtension = filename;
+                this.DirectoryInZip = directory;
+            }
+
+            public string Content { get; set; }
+
+            public string FileNameWithExtension { get; set; }
+
+            public string DirectoryInZip { get; set; }
+
+            // Hack field - synchronize the last modified date between result data and expected data
+            public DateTime LastModified { get; set; }
+        }
+
+        private string GetTestZipStreamBytesAsText(params FileZipModel[] zipModels)
         {
             string result;
             using (MemoryStream ms = new MemoryStream())
             using (ZipFile zipFile = new ZipFile())
             {
-                zipFile.Save(ms);
+                foreach (FileZipModel item in zipModels ?? Enumerable.Empty<FileZipModel>())
+                {
+                    zipFile.AddFileFromString(item.FileNameWithExtension, item.DirectoryInZip, item.Content);
+                    ZipEntry file = zipFile.Entries.FirstOrDefault(z => z.FileName.EndsWith(item.FileNameWithExtension, StringComparison.CurrentCultureIgnoreCase));
+                    file.LastModified = item.LastModified;
+                }
 
+                zipFile.Save(ms);
                 byte[] byteResult = ms.ToArray();
                 result = Convert.ToBase64String(byteResult);
             }
@@ -138,14 +260,24 @@ namespace Sitefinity.LibraryItemsDownloader.Tests.Services
             return result;
         }
 
-        private Expression<Func<LibraryItemsDownloadService, string>> GetDownloadableContentMethodExpession<TContent>() where TContent: MediaContent
+        private Expression<Func<LibraryItemsDownloadService, string>> GetDownloadableContentMethodExpession<TContent>() where TContent : MediaContent
         {
             return (service) => service.GetDownloadableContent<TContent>(It.IsAny<IEnumerable<DownloadLibaryItemRequestModel>>());
         }
 
         private Expression<Action<LibraryItemsDownloadService>> GetSaveLibraryItemsToStreamMethodExpression<TContent>() where TContent : MediaContent
         {
-            return (service) => service.SaveLibraryItemsToStream<TContent>(It.IsAny<HashSet<Guid>>(), It.IsAny<ZipFile>(), It.IsAny<string>()); ;
+            return (service) => service.SaveLibraryItemsToStream<TContent>(It.IsAny<HashSet<Guid>>(), It.IsAny<ZipFile>(), It.IsAny<string>());
+        }
+
+        private Expression<Action<LibraryItemsDownloadService>> GetSaveLibraryItemsToStreamMethodExpressionWithListFilter<TContent>(int countOfItems) where TContent : MediaContent
+        {
+            return (service) => service.SaveLibraryItemsToStream<TContent>(It.Is<HashSet<Guid>>(list => list.Count == countOfItems), It.IsAny<ZipFile>(), It.IsAny<string>());
+        }
+
+        private Expression<Action<LibraryItemsDownloadService>> GetSaveLibraryItemsToStreamRecursively<TContent>() where TContent : MediaContent
+        {
+            return (service) => service.SaveLibraryItemsToStreamRecursively<TContent>(It.IsAny<IFolder>(), It.IsAny<ZipFile>(), It.IsAny<string>());
         }
     }
 }
