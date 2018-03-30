0beb651db46f54cac6aeb7e59aa7c2fece655a5a
M-Yankov <cska31ultras@gmail.com>
2018-03-30 21:27:58 +0300

    Fixed assembly version to 9.1.6100

--- Sitefinity.LibraryItemsDownloader/Properties/AssemblyInfo.cs
+++ Sitefinity.LibraryItemsDownloader/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using Sitefinity.LibraryItemsDownloader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("9.1.6100")]
+[assembly: AssemblyFileVersion("9.1.6100")]
 [assembly: WebResource("Sitefinity.LibraryItemsDownloader.LibraryItemsDownloadService.js", "application/javascript")]
 [assembly: PreApplicationStartMethod(typeof(Installer), "PreApplicationStart")]
